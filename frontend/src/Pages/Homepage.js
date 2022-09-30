1df8a0718008002019aeaa39f0ebc9945d429828
sajdakabir <sajda.kbir.com>
2022-10-01 01:29:17 +0530

    start the login and signup page

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,11 +1,21 @@
 import React from 'react'
-
+import {Container, Box,Text} from '@chakra-ui/react';
 function Homepage() {
-  return (
-    <div>
-      home
-    </div>
-  )
+  return <Container>
+    <Box
+    d="flex"
+    justifyContent="center"
+    p={3}
+    textAlign="center"
+    bg={"white"}
+    w="100%"
+    m="40px 0 15px 0"
+    borderRadius="lg"
+    borderWidth="1px"
+    >
+      <Text fontSize="3xl" fontFamily="Roboto" >Chat A Lot</Text>
+    </Box>
+  </Container>
 }
 
 export default Homepage
