b33d455a16909b1949094d718f9958e0e064951e
sajdakabir <sajda.kbir.com>
2022-10-01 23:55:35 +0530

    login aoi done

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-
+const bcrypt=require('bcryptjs');
 const userSchema=mongoose.Schema(
     {
         name:{
@@ -24,6 +24,19 @@ const userSchema=mongoose.Schema(
         timestamps:true
     }
 );
+
+userSchema.methods.matchPassword=async function(enterPassword){
+    return await bcrypt.compare(enterPassword,this.password);
+};
+
+userSchema.pre('save',async function(next){
+if(!this.isModified){
+    next();
+}
+const salt=await bcrypt.genSalt(15);
+this.password=await bcrypt.hash(this.password,salt);
+});
+
 
 
 const User =mongoose.model('User',userSchema);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 const express =require('express');
-const {registerUser }=require('../controllers/userControllers');
+const {registerUser,authUser }=require('../controllers/userControllers');
 const router=express.Router();
 
 
 router.route('/').post(registerUser)
-
+router.route('/login').post(authUser)
 
 
 module.exports=router;
