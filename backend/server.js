28e97237fa51361d12031e9de7c09a7988a5752d
sajdakabir <sajda.kbir.com>
2022-10-02 00:14:07 +0530

    added the error handel middleware

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,20 @@ const dotenv=require('dotenv');
 const connectDB = require('./config/db');
 const colors =require('colors');
 const useRoutes=require('./routes/userRoutes');
+const{notFound,errorHandlers}=require('./middleware/errorMiddleware');
 dotenv.config();
 
 connectDB();
 const app=express();
 app.use(express.json());
+
+
 app.use('/api/user',useRoutes)
 
-
+app.use(notFound)
+app.use(errorHandlers)
 
 const port = process.env.PORT || 3001;
-
 app.listen(port, () => {
     console.log(`Server is up on port ${port}!`.yellow.bold);
   });
