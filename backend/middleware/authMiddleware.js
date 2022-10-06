fd4257142528cb0d37511c266c5470634ab1991e
sajdakabir <sajda.kbir.com>
2022-10-06 19:09:41 +0530

    fixed all the bugs

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,28 +1,28 @@
-// const jwr = require('jsonwebtoken');
-// const User = require('../models/userModel');
-// const asyncHandler = require('express-async-handler');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const asyncHandler = require('express-async-handler');
 
-// // const protectRoute = asyncHandler(async (req, res, next) => {
-// //     let token;
-// //     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-// //         try {
-// //             token = req.headers.authorization.split(" ")[1];
-// //             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-// //             req.user=await User.findById(decoded.id).select('-password');
-// //             next();
-// //         } catch (err) {
-// //             res.status(401);
-// //             throw new Error('not authorized, token failed');
-// //         }
+const protectRoute = asyncHandler(async (req, res, next) => {
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(" ")[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user=await User.findById(decoded.id).select('-password');
+            next();
+        } catch (err) {
+            res.status(401);
+            throw new Error('not authorized, token failed');
+        }
 
-// //     }
-// //     if(!token){
-// //         res.status(401);
-// //         throw new Error('not authorized, no token');
-// //     }
-// // });
+    }
+    if(!token){
+        res.status(401);
+        throw new Error('not authorized, no token');
+    }
+});
 
-// // module.exports={protectRoute};
+module.exports={protectRoute};
 
 
 
@@ -35,10 +35,10 @@
 //     ) {
 //       try {
 //         token = req.headers.authorization.split(" ")[1];
+        
   
 //         //decodes token id
 //         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  
 //         req.user = await User.findById(decoded.id).select("-password");
   
 //         next();
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,13 @@
 const express =require('express');
 const {registerUser,authUser ,allUsers}=require('../controllers/userControllers');
-const {protect}=require('../middleware/authMiddleware');
-// const {protectRoute}=require('../middleware/authMiddleware');
+const {protectRoute}=require('../middleware/authMiddleware');
 
 const router=express.Router();
 
 
 router.route('/')
 .post(registerUser)
-.get(protect,allUsers);
-// .get(protectRoute,allUsers);
+.get(protectRoute,allUsers);
 
 
 router.route('/login').post(authUser);
