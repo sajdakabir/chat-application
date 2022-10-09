c50ce84ef450a1374ad2bd0883894ede32bacb7d
sajdakabir <sajda.kbir.com>
2022-10-09 18:35:48 +0530

    added one to one chat

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -46,24 +46,27 @@ const accessChat=asyncHandler(async(req,res)=>{
 
 
 
-const fetchChats = asyncHandler(async (req, res) => {
-    try {
-      Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
-        .populate("users", "-password")
-        .populate("groupAdmin", "-password")
-        .populate("latestMessage")
-        .sort({ updatedAt: -1 })
-        .then(async (results) => {
-          results = await User.populate(results, {
-            path: "latestMessage.sender",
-            select: "name pic email",
-          });
-          res.status(200).send(results);
-        });
-    } catch (error) {
-      res.status(400);
-      throw new Error(error.message);
-    }
-  });
+const fetchChats=asyncHandler(async(req,res)=>{
+try {
 
-module.exports={accessChat};
+    Chat.find({users:{$elemMatch:{$eq:req.user._id}}})
+    .populate('users','-password')
+    .populate('groupAdmin','-password')
+    .populate('latestMessage')
+    .sort({updatedAt:-1})
+    .then(async(results)=>{
+        results=await User.populate(results,{
+            path:'latestMessage.sender',
+            select:' name profile email',
+        });
+        res.status(200).send(results);
+    });
+
+    
+} catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+}
+});
+
+module.exports={accessChat,fetchChats};
