d0a5f95e5a9e3c9dd7e2b568878fd03b61046fe0
sajdakabir <sajda.kbir.com>
2022-10-12 02:11:44 +0530

    implemented the login

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,14 +1,69 @@
 import React, { useState } from 'react'
-import { VStack, FormControl, FormLabel, Input, InputRightElement, Button, InputGroup } from '@chakra-ui/react'
+import { VStack, FormControl, FormLabel, Input, InputRightElement, Button, InputGroup } from '@chakra-ui/react';
+import axios from "axios";
+import { useToast } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
+
 function Login() {
 
     const [show, setShow] = useState(false);
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
-   
-
+    const [loading, setLoading] = useState(false);
+  
     const handleClick = () => setShow(!show);
-    const submitHandler = () => { };
+    const toast = useToast();
+    const history = useHistory();
+    const submitHandler =async () => {
+      setLoading(true);
+      if(!email || !password){
+        toast({
+          title: "Please Fill all the Feilds",
+          status: "warning",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom",
+        });
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const config={
+          headers: {
+            "Content-type": "application/json",
+          },
+        };
+
+        const { data } = await axios.post(
+          "/api/user/login",
+          { email, password },
+          config
+        );
+
+        toast({
+          title: "Login Successful",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom",
+        });
+        localStorage.setItem("userInfo", JSON.stringify(data));
+        setLoading(false);
+        history.push("/chats");
+      } catch (error) {
+        toast({
+          title: "Error Occured!",
+          description: error.response.data.message,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom",
+        });
+        setLoading(false);
+      }
+
+     };
 
 
   return (
@@ -43,6 +98,7 @@ function Login() {
       width="100%"
       style={{ marginTop: 15 }}
       onClick={submitHandler}
+      isLoading={loading}
     >
       Login
     </Button>
