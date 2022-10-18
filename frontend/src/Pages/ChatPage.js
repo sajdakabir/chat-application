a4c6319f83882c1ca99dd899610bfd56c93f1881
sajdakabir <sajda.kbir.com>
2022-10-18 11:58:30 +0530

    changes in ChatPage

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -2,27 +2,14 @@ import {ChatState} from '../Context/ChatProvider';
 import {Box} from '@chakra-ui/layout';
 import SideDrawer from '../components/miscellaneous/SideDrawer';
 import MyChats from '../components/MyChats';
-import ChatBox from '../components/ChatBox'
+import ChatBox from '../components/ChatBox';
+import { useState } from "react";
 
 const Chatpage = () => {
  
   const { user } = ChatState();
+  const [fetchAgain, setFetchAgain] = useState(false);
 
-//   return (
-//     <div style={{ width: "100%" }}>
-//       {user && <SideDrawer />}
-//       <Box d="flex" 
-//       disply='flex'
-//       justifyContent="space-between"
-//        w="100%"
-//         h="91.5vh"
-//          p="10px">
-//         {user && <MyChats/>}
-//         {user && <ChatBox/>}
-//       </Box>
-//     </div>
-//   );
-// };
 
 
 
@@ -30,9 +17,9 @@ return (
   <div style={{ width: "100%" }}>
     {user && <SideDrawer />}
     <Box display='flex' d='flex' justifyContent='space-between' w='100%' h='91.5vh' p='10px'>
-      {user && <MyChats />}
+      {user && <MyChats fetchAgain={fetchAgain} />}
       {user && (
-        <ChatBox />
+        <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
       )}
     </Box>
   </div>
