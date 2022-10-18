dd9d43500fd214d7f9b09c228b9612cc6663c53a
sajdakabir <sajda.kbir.com>
2022-10-18 16:03:49 +0530

    added the single message

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
-import {BrowserRouter}from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import ChatProvider from './Context/ChatProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChatProvider>
-    <BrowserRouter>
-    <ChakraProvider>
-     <App />
-     </ChakraProvider>
-    </BrowserRouter>
-     
-    
-  </ChatProvider>
+
+  <BrowserRouter>
+    <ChatProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ChatProvider>
+  </BrowserRouter>
+
+
+
 );
 
 
