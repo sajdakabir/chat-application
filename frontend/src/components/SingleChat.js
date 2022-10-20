ebc9b6becc3146aac69d5064be2489fd69afad31
sajdakabir <sajda.kbir.com>
2022-10-20 19:52:46 +0530

    developed 🔥

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -16,8 +16,8 @@ import io from "socket.io-client";
 import UpdateGroupChatModal from './miscellaneous/UpdateGroupChatModal';
 import { ChatState } from "../Context/ChatProvider";
 
-const ENDPOINT = "https://chat-a-lot-mern.herokuapp.com/"; 
-// const ENDPOINT = "http://localhost:3001"; 
+// const ENDPOINT = "https://chat-a-lot-mern.herokuapp.com/"; 
+const ENDPOINT = "http://localhost:3001"; 
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
