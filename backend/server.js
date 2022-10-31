d0c7f60e0afffd89240fc154059664bb2e47e71a
sajdakabir <sajda.kbir.com>
2022-10-31 23:25:22 +0530

    developed🔥

--- backend/server.js
+++ backend/server.js
@@ -22,25 +22,9 @@ app.use('/api/message',messageRoutes);
 
 
 
-const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-
-
-
-
-app.use(notFound)
-app.use(errorHandlers)
+// app.use(notFound)
+// app.use(errorHandlers)
 
 const PORT = process.env.PORT;
 
